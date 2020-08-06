fddd2d02decf4fce24028ab9e2624c799a0c0d0c
Nathan Regaudie <nathanregaudie@gmail.com>
2020-08-06 21:10:18 +0200

    MAJ src/refresh.py

--- src/refresh.py
+++ src/refresh.py
@@ -6,15 +6,21 @@ from calcul import *
 from sort import *
 import files
 
-with open("ACCOUNTS_PATH.txt", "r", encoding="utf-8") as _file:
-    accounts_path = _file.readline().strip("\n") + "\\"
+def refresh(modes=[], verbose=0):
+    with open("ACCOUNTS_PATH.txt", "r", encoding="utf-8") as _file:
+        accounts_path = _file.readline().strip("\n") + "\\"
 
-actfile = whatfile()
-filecontent = files.reader(accounts_path + actfile)
+    actfile = whatfile()
+    filecontent = files.reader(accounts_path + actfile)
 
-dep = float(filecontent[0][1])
-filecontent[-1][1] = "{:.2f}".format(calcul_total(dep, filecontent[3:len(filecontent) - 2]))
-filecontent[3:len(filecontent) - 2] = sort_by_dates(filecontent[3:len(filecontent) - 2])
-files.writer(accounts_path + actfile, filecontent)
+    dep = float(filecontent[0][1])
+    filecontent[3:len(filecontent) - 2] = sort_by_dates(filecontent[3:len(filecontent) - 2])
+    filecontent[-1][1] = "{:.2f}".format(calcul_total(dep, filecontent[3:len(filecontent) - 2], modes))
+    files.writer(accounts_path + actfile, filecontent)
 
-print(actfile, "mis à jour.")
+    if verbose:
+        print(actfile, "mis à jour.")
+
+
+if __name__ == "__main__":
+    refresh(verbose=1)
