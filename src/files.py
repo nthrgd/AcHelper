3edbab0a2b5ed553bdff8060404c15a21b3c1deb
Nathan Regaudie <nathanregaudie@gmail.com>
2020-08-06 18:35:30 +0200

    Ajout du paramètre 'delimiter' pour les fonctions du module files

--- src/files.py
+++ src/files.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def reader(filename):
+def reader(filename, delimiter=";"):
     with open(filename, "r", encoding="utf-8") as file:
         content = []
         for line in file.readlines():
-            line = line.replace("\n", "").split(";")
+            line = line.replace("\n", "").split(delimiter)
             if not line[-1]: # Si line[-1] est une chaine vide
                 del line[-1]
             content.append(line)
         return content
 
-def writer(filename, content):
+def writer(filename, content, delimiter=";"):
     with open(filename, "w", encoding="utf-8") as file:
         for line in content:
-            file.write(";".join(line) + "\n")
+            file.write(delimiter.join(line) + "\n")
