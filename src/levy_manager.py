92198dbc331f04d1879f796af57df3a5cc4119dd
Nathan Regaudie <nathanregaudie@gmail.com>
2020-08-07 03:00:44 +0200

    Réécriture de levy_manager.py

--- src/levy_manager.py
+++ src/levy_manager.py
@@ -1,10 +1,11 @@
 import files
+from date import Date
 from what import whatfile
 from sort import sort_by_dates
 from refresh import refresh
 
 
-with open("ACCOUNTS_PATH.txt", "r", encoding="utf-8") as _file:
+with open("../config/ACCOUNTS_PATH.txt", "r", encoding="utf-8") as _file:
     accounts_path = _file.readline().strip("\n")
 levysfile = accounts_path + "/NE PAS SUPPRIMER/Prélèvements.txt"
 
@@ -15,9 +16,12 @@ levys_modes = []
 # levys.autoremove()
 
 for i in range(len(levys)):
-    levys[i][0], levys[i][1], levys[i][2] = levys[i][2], levys[i][0].upper(), "{:.2f}".format(float(levys[i][1]))
     for j in range(3):
         levys[i][j] = levys[i][j].replace(" ", "")
+
+    # On met les lignes au format utilisé par AcHelper
+    levys[i][0], levys[i][1], levys[i][2] = Date(levys[i][0] + "/").whatdate(), levys[i][1].upper(), "{:.2f}".format(float(levys[i][2]))
+
     # Ajout du mode après la suppression des espaces
     levys_modes.append(levys[i][1])
 
