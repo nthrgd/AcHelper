e9c7d0cc64af2695822f604f584c6da074ca6828
Nathan Regaudie <nathanregaudie@gmail.com>
2020-08-11 23:45:54 +0200

    src/table.py: Add a new method: autocomplete()
    This method involve to auto-completing the account lines
    of the table.

--- src/table.py
+++ src/table.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+from date import Date
+from mode import Mode
 
 class Table:
 
@@ -7,3 +9,19 @@ class Table:
         self.starting = content[0][1]
         self.total = content[-1][1]
         self.accounts = content[3:len(content) - 2]
+
+
+    def autocomplete(self):
+        """
+        Autocomplete the columns of self.accounts and update self.all.
+        """
+        for i in range(len(self.accounts)):
+            date, mode, amount = self.accounts[i]
+
+            self.accounts[i][0] = Date(date).whatdate()          # Date
+            self.accounts[i][1] = Mode(mode).whatmode()          # Mode
+            self.accounts[i][2] = "{:.2f}".format(float(amount)) # Amount
+
+
+        # Update self.all
+        self.all[3:len(self.all) - 2] = self.accounts
