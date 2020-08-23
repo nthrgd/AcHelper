f8783113013d344cae0e932cdca891411231965f
Nathan Regaudie <nathanregaudie@gmail.com>
2020-08-23 17:34:20 +0200

    Error handling for modify.sh and refresh.sh

--- bin/scripts/modify.sh
+++ bin/scripts/modify.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/bash
-cd ../../src
-if [ -e /usr/bin/python3 ]; then
-    ./main.py
+
+red="\e[0;31m"
+neutral="\e[0;m"
+
+if [ -e ../../config/actfile.txt ]; then
+    if [ -e $(cat ../../config/accounts_path.txt)/$(cat ../../config/actfile.txt) ]; then
+        cd ../../src
+        if [ -e /usr/bin/python3 ]; then
+            ./main.py
+        else
+            python ./main.py
+            read end
+        fi
+    else
+        echo -e "${red}Erreur:${neutral} Le fichier de compte le plus récent n'existe plus. Veuillez en recréer un nouveau ou restorer ce dernier."
+    fi
+
 else
-    python ./main.py
-    read end
+    echo -e "${red}Erreur:${neutral} Aucun fichier de compte n'a encore été créer."
 fi
--- bin/scripts/refresh.sh
+++ bin/scripts/refresh.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/bash
-cd ../../src
-if [ -e /usr/bin/python3 ]; then
-    ./refresh.py
+
+red="\e[0;31m"
+neutral="\e[0;m"
+
+if [ -e ../../config/actfile.txt ]; then
+    if [ -e $(cat ../../config/accounts_path.txt)/$(cat ../../config/actfile.txt) ]; then
+        cd ../../src
+        if [ -e /usr/bin/python3 ]; then
+            ./refresh.py
+        else
+            python ./refresh.py
+        fi
+    else
+        echo -e "${red}Erreur:${neutral} Le fichier de compte le plus récent n'existe plus. Veuillez en recréer un nouveau ou restorer ce dernier."
+    fi
+
 else
-    python ./refresh.py
+    echo -e "${red}Erreur:${neutral} Aucun fichier de compte n'a encore été créer."
 fi
